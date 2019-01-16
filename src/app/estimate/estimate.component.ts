fba079a690b20f8863d9e315254b74a38d048640
Chocolaterules <marie.boulez@laposte.net>
2019-01-16 09:22:15 +0100

    Ajout doc et css

--- src/app/estimate/estimate.component.ts
+++ src/app/estimate/estimate.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
-
+/**
+ * Composant utilisé pour la demande d'un devis par un utilisateur.
+ */
 @Component({
   selector: 'app-estimate',
   templateUrl: './estimate.component.html',
@@ -13,6 +15,9 @@ export class EstimateComponent implements OnInit {
   ngOnInit() {
   }
 
+  /**
+   * Methode permettant la redirection vers Wikipedia lorsque l'utilisateur clique sur le bouton "Par téléphone" ou "Par Email".
+   */
   goToUrl(): void {
     this.document.location.href = 'https://fr.wikipedia.org/';
   }
--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -4,7 +4,9 @@ import { Answer } from '../answer';
 import { Survey } from '../survey';
 import { NgForm } from '@angular/forms';
 import { Client } from '../client';
-
+/**
+ * COmposant utilisé pour afficher les formulaires d'enregistrement des commentaires postés par les utilisateurs.
+ */
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
@@ -44,8 +46,15 @@ export class SurveyComponent implements OnInit {
     this.valeur = 2;
   }
 
-
+  /**
+   * Methode permettant d'enregistrer un commentaire négatif pour le sondage en cours.
+   * @param myForm Le formulaire d'enregistrement du commentaire.
+   */
   validateNegCom(myForm: NgForm) {
+    // Création d'une nouvelle réponse avec l'id du sondage en cours.
+    // Obligation de passer par un objet Survey contenant juste un attribut id rempli avec l'id du sondage en cours
+    // pour que le WebService puisse stocker l'information dans la colonne survey_id en BDD.
+    // On fixe isPositive à false directement.
     const newAnswer = new Answer(
       null, false, null,
       new Survey(this.surveyId, null, null, null),
@@ -54,40 +63,58 @@ export class SurveyComponent implements OnInit {
     this.service.createAnswer(newAnswer).subscribe(
       (answerFromJee) => {
         if (answerFromJee.id != null) {
+          // tslint:disable-next-line:max-line-length
+          // Une fois que le servuer a renvoyé la réponse enregistrée en BDD avec un id, on bascule sur l'affichage du message de remerciement
+          // avec valeur = 3.
           this.endMessage = 'Merci pour votre participation !';
-          // this.service.isEnd = true;
           this.valeur = 3;
         }
       });
   }
-
+  /**
+     * Methode permettant d'enregistrer un commentaire positif pour le sondage en cours.
+     * @param myForm Le formulaire d'enregistrement du commentaire.
+     */
   validatePosCom(id: number) {
+    // Création d'une nouvelle réponse avec l'id du sondage en cours.
+    // Obligation de passer par un objet Survey contenant juste un attribut id rempli avec l'id du sondage en cours
+    // pour que le WebService puisse stocker l'information dans la colonne survey_id en BDD.
+    // On fixe isPositive à true directement.
     const newAnswer = new Answer(
       null, true, new Client(id, null, null, null, null, null),
-      new Survey(this.surveyId, null, null, null), null, null
-    );
+      new Survey(this.surveyId, null, null, null),
+      null, null);
     this.service.createAnswer(newAnswer).subscribe(
       (answerFromJee) => {
         if (answerFromJee.id != null) {
+          // Si la réponse renvoyée par le serveur existe bien, on appelle setDays() qui demande au WebService le nombre de jours
+          // entre la date du jour et la fin prévisionnelle du sondage en cours.
           this.setDays();
         }
       });
   }
-
+  /**
+   * Methode utilisant le webService pour connaitre le nombre de jours restants avant la fin prévisionnelle du sondage en cours.
+   * Si le nombre de jours restant vaut 1 ou 0, affichage de "quelques jours" à l'utilisateur au lieu de "0/1 jours"
+   */
   setDays() {
     this.service.getDays().subscribe(
       (numberDays) => {
         this.days = numberDays;
-        const numDays = this.days > 3 ? this.days : 'quelques';
+        const numDays = this.days > 1 ? this.days : 'quelques';
         // tslint:disable-next-line:max-line-length
         this.endMessage = `Merci ! Vous serez contacté dans un délai de ${numDays} jours par un de nos conseillers !`;
-        // this.service.isEnd = true;
         this.valeur = 3;
       }
     );
   }
 
-
+  /**
+   * Methode permettant d'enregistrer un nouveau client ayant posté un avis positif pour le sondage en cours.
+   * Le client a forcément enregistré soit un email soit un numéro de téléphone.
+   * @param client Le client à sauvegarder en BDD.
+   *
+   */
   createClient(client: Client) {
     const newClient = new Client(null, client.firstName, client.lastName,
       client.telNum, client.email, null);
